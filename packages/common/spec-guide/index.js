5d91151cf89e38320149cb09a3d4660de2b1546d
Brian Miller <brian@parameter1.com>
2021-08-17 15:53:56 -0500

    rename env variable

--- packages/common/spec-guide/index.js
+++ packages/common/spec-guide/index.js
@@ -1,11 +1,11 @@
 const { asyncRoute } = require('@parameter1/base-cms-utils');
 const fetch = require('node-fetch');
 
-const { GOOGLE_DOC_API_KEY } = process.env;
+const { SPEC_GUIDE_DOCS_API_KEY } = process.env;
 module.exports = (app) => {
   app.use('/__spec-guide', asyncRoute(async (req, res) => {
     const { src } = req.query;
-    const url = `${src}?key=${GOOGLE_DOC_API_KEY}`;
+    const url = `${src}?key=${SPEC_GUIDE_DOCS_API_KEY}`;
     try {
       const response = await fetch(url);
       const json = await response.json();
