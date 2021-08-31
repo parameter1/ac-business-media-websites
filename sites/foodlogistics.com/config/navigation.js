95a36cb1c1939a3c9b587e708cf75bf0b98fe3e0
Jessica Wade <jessica.wade@parameter1.com>
2021-08-31 10:40:41 -0500

    Fix broken link

--- sites/foodlogistics.com/config/navigation.js
+++ sites/foodlogistics.com/config/navigation.js
@@ -178,9 +178,9 @@ module.exports = {
       tertiary: { items: tertiaryItems },
       primary: {
         items: [
-          { href: '/podcasts/link', label: 'L.I.N.K' },
+          { href: 'https://cms.megaphone.fm/channel/supplychain', label: 'L.I.N.K', target: '_blank' },
           { href: '/videos/link-live', label: 'L.I.N.K LIVE' },
-          { href: '/podcasts/link-educate', label: 'L.I.N.K Educate' },
+          { href: 'https://cms.megaphone.fm/channel/linkeducate', label: 'L.I.N.K Educate', target: '_blank' },
         ],
       },
     },
