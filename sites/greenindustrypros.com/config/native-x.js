4b7553189d1584545dea3a3db8787a986696c91e
Shinsina <shinsina@att.net>
2021-09-22 09:50:12 -0500

    Realias native-x channels appropriately

--- sites/greenindustrypros.com/config/native-x.js
+++ sites/greenindustrypros.com/config/native-x.js
@@ -2,8 +2,8 @@ module.exports = {
   placements: {
     default: '5bed868f8e1d72000126d058',
     'mowing-maintenance': '5bed845f8e1d72000126c340',
-    'lawn-care-renovation': '5bed84258e1d72000126c1c4',
-    'snow-ice-management': '5bed813a8e1d72000126aef6',
+    'lawn-care': '5bed84258e1d72000126c1c4',
+    snowpro: '5bed813a8e1d72000126aef6',
     'design-installation': '5bed83fbf3eec100015e9180',
     'irrigation-water-management': '5bed83d6f3eec100015e9068',
   },
