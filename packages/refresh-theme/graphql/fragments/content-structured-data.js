cbebcc444abbf7d77bc4b826f93b4a899c5219fd
Brian Miller <brian@parameter1.com>
2021-10-29 09:38:42 -0500

    add primary section back in

--- packages/refresh-theme/graphql/fragments/content-structured-data.js
+++ packages/refresh-theme/graphql/fragments/content-structured-data.js
@@ -3,6 +3,10 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 fragment WebsiteContentStructuredDataFragment on Content {
+  primarySection {
+    id
+    name
+  }
   taxonomy {
     edges {
       node {
