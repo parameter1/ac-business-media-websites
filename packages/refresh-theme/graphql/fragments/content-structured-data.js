5372b1756c24c00b9d681e80d92267049ebfa3f7
Brian Miller <brian@parameter1.com>
2021-10-29 09:32:25 -0500

    Update fragment with additional fields instead of all

--- packages/refresh-theme/graphql/fragments/content-structured-data.js
+++ packages/refresh-theme/graphql/fragments/content-structured-data.js
@@ -3,51 +3,6 @@ const gql = require('graphql-tag');
 module.exports = gql`
 
 fragment WebsiteContentStructuredDataFragment on Content {
-  id
-  type
-  siteContext {
-    path
-    canonicalUrl
-    noIndex
-  }
-  primarySection {
-    id
-    name
-  }
-  published
-  updated
-  metadata {
-    title
-    description
-    publishedDate
-    updatedDate
-    expiresDate
-    image {
-      id
-      src(input: { options: { auto: "format,compress", w: "1200", fit: "max", q: 70 } })
-    }
-  }
-  ... on ContentVideo {
-    embedSrc
-  }
-  ... on Authorable {
-    authors {
-      edges {
-        node {
-          id
-          name
-        }
-      }
-    }
-  }
-  images(input:{ pagination: { limit: 0 }, sort: { order: values } }) {
-    edges {
-      node {
-        id
-        src(input: { options: { auto: "format,compress", w: "1200", fit: "max", q: 70 } })
-      }
-    }
-  }
   taxonomy {
     edges {
       node {
