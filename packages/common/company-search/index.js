ce5a5a752cff9fbd77f6b4618282e60c262e7eff
Brian Miller <brian@parameter1.com>
2021-11-16 09:10:46 -0600

    Update to use jsonErrorHandler in company search route

--- packages/common/company-search/index.js
+++ packages/common/company-search/index.js
@@ -1,29 +1,26 @@
 const loader = require('@parameter1/base-cms-marko-web-search/loaders/search');
+const jsonErrorHandler = require('@parameter1/base-cms-marko-web/express/json-error-handler');
 const { asyncRoute } = require('@parameter1/base-cms-utils');
 
 const queryFragment = require('../graphql/fragments/company-search');
 
 module.exports = (app) => {
-  app.use('/__company-search', asyncRoute(async (req, res) => {
+  app.get('/__company-search', asyncRoute(async (req, res) => {
     const { searchQuery } = req.query;
     const { apollo, $baseBrowse } = res.locals;
 
-    try {
-      const response = await loader(
-        {
-          apolloBaseCMS: apollo,
-          apolloBaseBrowse: $baseBrowse,
-        },
-        {
-          status: 1,
-          contentTypes: ['COMPANY'],
-          searchQuery,
-          queryFragment,
-        },
-      );
-      res.send(response);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  }));
+    const response = await loader(
+      {
+        apolloBaseCMS: apollo,
+        apolloBaseBrowse: $baseBrowse,
+      },
+      {
+        status: 1,
+        contentTypes: ['COMPANY'],
+        searchQuery,
+        queryFragment,
+      },
+    );
+    res.json(response);
+  }), jsonErrorHandler());
 };
