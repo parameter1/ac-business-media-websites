6cae5d4251c68d8f30642d706cc26adfe8e9c7c3
Shinsina <46794001+Shinsina@users.noreply.github.com>
2022-02-02 17:08:06 -0600

    Move publicationIds under magazines property

--- sites/sdcexec.com/config/site.js
+++ sites/sdcexec.com/config/site.js
@@ -61,6 +61,7 @@ module.exports = {
     userName: 'acbm',
   },
   magazines: {
+    publicationIds: ['53c9604b1784f8066eb2c8b9'],
     description: '',
   },
   contactUs: {
@@ -87,5 +88,4 @@ module.exports = {
       imgSrc: 'https://img.sdcexec.com/files/base/acbm/sdce/image/static/sdc-leaders.png?h=85&auto=format,compress',
     },
   },
-  publicationIds: ['53c9604b1784f8066eb2c8b9'],
 };
