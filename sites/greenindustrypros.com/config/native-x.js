59c0b12b2ecebd965ae75d9e274d371f76b16a52
Brian Miller <brian@parameter1.com>
2022-02-21 14:29:13 -0600

    Add 2 new topic placements for nativeX

--- sites/greenindustrypros.com/config/native-x.js
+++ sites/greenindustrypros.com/config/native-x.js
@@ -1,10 +1,12 @@
 module.exports = {
   placements: {
     default: '5bed868f8e1d72000126d058',
+    'business-management': '6213f44e1ab23700016c479d',
     'mowing-maintenance': '5bed845f8e1d72000126c340',
     'lawn-care': '5bed84258e1d72000126c1c4',
     snowpro: '5bed813a8e1d72000126aef6',
     'design-installation': '5bed83fbf3eec100015e9180',
     'irrigation-water-management': '5bed83d6f3eec100015e9068',
+    'tree-ornamental': '6213f48aee72d80001eee5a1',
   },
 };
