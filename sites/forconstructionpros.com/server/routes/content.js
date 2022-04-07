747d4627bc46945e46169c8e6a46c489e5e32a8d
Brandon Krigbaum <brandonbk@gmail.com>
2022-04-07 18:50:07 -0500

    Add userState middleware to content routes

--- sites/forconstructionpros.com/server/routes/content.js
+++ sites/forconstructionpros.com/server/routes/content.js
@@ -1,4 +1,5 @@
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
+const userState = require('@ac-business-media/refresh-theme/middleware/user-state');
 const content = require('@ac-business-media/refresh-theme/templates/content');
 // const contact = require('@ac-business-media/refresh-theme/templates/content/contact');
 const company = require('@ac-business-media/refresh-theme/templates/content/company');
