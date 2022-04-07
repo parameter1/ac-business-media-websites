7240b9776337d67f251ae3475732daa37dc2a183
Brandon Krigbaum <brandonbk@gmail.com>
2022-04-07 18:52:51 -0500

    Remove userState from mixequipmentmag.com

--- sites/mixequipmentmag.com/server/routes/content.js
+++ sites/mixequipmentmag.com/server/routes/content.js
@@ -3,7 +3,7 @@ const content = require('../templates/content');
 const queryFragment = require('../graphql/fragments/content-page');
 
 module.exports = (app) => {
-  app.get('/*?:id(\\d{8})*', userState(), withContent({
+  app.get('/*?:id(\\d{8})*', withContent({
     template: content,
     queryFragment,
   }));
