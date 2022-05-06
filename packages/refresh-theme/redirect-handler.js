7ac118c4e607014a64b1ad8c9f4268b6ea7dd519
Josh Worden <josh@parameter1.com>
2022-05-06 15:56:44 -0500

    Update redirect handler to support deleted ids

--- packages/refresh-theme/redirect-handler.js
+++ packages/refresh-theme/redirect-handler.js
@@ -1,4 +1,6 @@
-module.exports = ({ from }) => {
+const redirectIds = require('./redirect-ids.json');
+
+const feeds = ({ from }) => {
   const feedPattern = /^\/feeds\/([a-z0-9-/]+)/;
   const matches = feedPattern.exec(from);
   if (!matches || !matches[1]) return null;
@@ -7,3 +9,13 @@ module.exports = ({ from }) => {
   const to = `/__rss/website-scheduled-content.xml?input=%7B%22sectionAlias%22%3A%22${encodeURIComponent(alias)}%22%7D `;
   return { to };
 };
+
+const ids = ({ from }) => {
+  const match = Object.keys(redirectIds).find(id => new RegExp(id).test(from));
+  return match && { to: redirectIds[match] };
+};
+
+module.exports = ({ from }) => [
+  feeds,
+  ids,
+].reduce((v, handler) => v || handler({ from }), null);
