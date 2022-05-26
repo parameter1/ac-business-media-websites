c24ccb86839d673b6880009138269978330d4a0f
Brian Miller <brian@parameter1.com>
2022-05-26 08:56:43 -0500

    convert contacts us to use base-cms pacakge
    @requires Dep Upgrade and will require conversion to recaptcha V3

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,4 +1,3 @@
-const ContactUsForm = () => import(/* webpackChunkName: "common-contact-us-form" */ './contact-us-form.vue');
 const AutoScroll = () => import(/* webpackChunkName: "common-auto-scroll" */ './auto-scroll.vue');
 const CommonCompanySearch = () => import(/* webpackChunkName: "common-company-search" */ './company-search.vue');
 const CommonSectionSearch = () => import(/* webpackChunkName: "common-section-search" */ './section-search.vue');
@@ -6,8 +5,6 @@ const SpecGuideTable = () => import(/* webpackChunkName: "common-spec-guide-tabl
 
 export default (Browser) => {
   const { EventBus } = Browser;
-  // @todo this should be removed once contact us is moved to core.
-  Browser.register('CommonContactUsForm', ContactUsForm);
   Browser.register('CommonAutoScroll', AutoScroll);
   Browser.register('CommonSpecGuideTable', SpecGuideTable);
   Browser.register('CommonCompanySearch', CommonCompanySearch, {
