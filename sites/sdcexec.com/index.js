2c9e8c16969ee351efd937f9f60b66aefb72a616
Brian Miller <brian@parameter1.com>
2022-06-01 16:02:21 -0500

    Sample of site level content gating.

--- sites/sdcexec.com/index.js
+++ sites/sdcexec.com/index.js
@@ -4,6 +4,7 @@ const cufv1 = require('@ac-business-media/refresh-theme/redirect-handler-cufv1')
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
+const contentGating = require('./utils/content-gating');
 
 const { log } = console;
 
@@ -13,4 +14,5 @@ module.exports = startServer({
   siteConfig,
   routes,
   redirectHandler: cufv1('update.sdcexec.com'),
+  contentGating,
 }).then(() => log('Website started!')).catch(e => setImmediate(() => { throw e; }));
