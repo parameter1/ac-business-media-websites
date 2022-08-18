980cc20912e5d2981ea677b7fa75e50cdb25efe4
Brian Miller <brian@parameter1.com>
2022-08-18 08:54:38 -0500

    Add omedaIdX confic to FL

---
+++ sites/foodlogistics.com/config/omeda-identity-x.js
@@ -0,0 +1,10 @@
+const configure = require('@ac-business-media/refresh-theme/config/omeda-identity-x');
+const configureOmeda = require('@ac-business-media/refresh-theme/config/omeda');
+const idxConfig = require('./identity-x');
+
+module.exports = configure({
+  omedaConfig: configureOmeda({}),
+  idxConfig,
+  rapidIdentProductId: 15374,
+  websiteBehaviorAttributeId: 451158,
+});
--- sites/foodlogistics.com/config/omeda.js
+++
@@ -1,5 +0,0 @@
-const configure = require('@ac-business-media/refresh-theme/config/omeda');
-
-module.exports = configure({
-  rapidIdentification: { productId: 15374 },
-});
