80a129a8db22c066beecad480a822133f954b120
Brian Miller <brian@parameter1.com>
2022-08-23 15:00:16 -0500

    Append site demographic ids with hooks
    Apply the demoIds for Submitted, Verifiied and Full Profile per site.

--- sites/sdcexec.com/config/omeda-identity-x.js
+++ sites/sdcexec.com/config/omeda-identity-x.js
@@ -7,4 +7,21 @@ module.exports = configure({
   idxConfig,
   rapidIdentProductId: 15356,
   websiteBehaviorAttributeId: 451160,
+  appendDemographicToHook: [
+    {
+      hook: 'onLoginLinkSent',
+      demographicId: 5081074, // Email Authorization Status
+      valueIds: [5104240], // Submitted
+    },
+    {
+      hook: 'onAuthenticationSuccess',
+      demographicId: 5081074,
+      valueIds: [5104240, 5104239], // Submitted, Verified
+    },
+    {
+      hook: 'onUserProfileUpdate',
+      demographicId: 5081074,
+      valueIds: [5104240, 5104239, 5104238], // Submitted, Verified, Full Profile
+    },
+  ],
 });
