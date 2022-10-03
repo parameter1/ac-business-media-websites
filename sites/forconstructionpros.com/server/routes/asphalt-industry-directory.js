98dc6cb7745852b18cfe935920536c3c6f378035
Shinsina <shinsina@att.net>
2022-10-03 08:27:26 -0500

    Remove related template routing

--- sites/forconstructionpros.com/server/routes/asphalt-industry-directory.js
+++
@@ -1,5 +0,0 @@
-const template = require('../templates/asphalt-industry-directory');
-
-module.exports = (app) => {
-  app.get('/asphalt-industry-directory', (_, res) => { res.marko(template); });
-};
