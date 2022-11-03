21e6f301fff7bffc7cae9d1fedc109f229488aeb
Brian Miller <brian@parameter1.com>
2022-11-03 09:00:36 -0500

    Add new useLinkInjectionBody to content page fragment
    No long sideload the bcl content body, but rather request the content body with an input of useLinkInjectedBody: true in order to return the bcl version of the body no saved on base-cms content.

--- sites/forconstructionpros.com/server/routes/content.js
+++ sites/forconstructionpros.com/server/routes/content.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-const newrelic = require('newrelic');
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
 const userState = require('@ac-business-media/refresh-theme/middleware/user-state');
 const content = require('@ac-business-media/refresh-theme/templates/content');
@@ -10,41 +8,12 @@ const queryFragment = require('@ac-business-media/refresh-theme/graphql/fragment
 const companyQueryFragment = require('@ac-business-media/refresh-theme/graphql/fragments/content-company-page');
 
 module.exports = (app) => {
-  const { site } = app.locals;
-  const enableIronProsLinking = site.get('enableIronProsLinking');
-
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
     queryFragment: companyQueryFragment,
   }));
-  if (enableIronProsLinking) {
-    app.get('/*?:id(\\d{8})*', userState(), withContent({
-      template: content,
-      queryFragment,
-      async sideloadDataFn() {
-        try {
-          const res = await fetch('https://bcl.tauron.base.parameter1.com/', {
-            method: 'POST',
-            headers: { 'content-type': 'application/json', 'x-tenant-key': 'acbm_fcp' },
-            body: JSON.stringify({
-              query: `query { highlightedContentBody(input: { _id: ${this.variables.input.id} }) }`,
-            }),
-          });
-          const { data } = await res.json();
-          if (!data || !data.highlightedContentBody) return {};
-          return {
-            bcl: { body: data.highlightedContentBody },
-          };
-        } catch (e) {
-          newrelic.noticeError(e);
-          return {};
-        }
-      },
-    }));
-  } else {
-    app.get('/*?:id(\\d{8})*', userState(), withContent({
-      template: content,
-      queryFragment,
-    }));
-  }
+  app.get('/*?:id(\\d{8})*', userState(), withContent({
+    template: content,
+    queryFragment,
+  }));
 };
