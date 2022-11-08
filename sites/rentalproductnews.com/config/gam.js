d637e4ea494ff4a317aa3ba75be651c9dcdf2a6b
Jessica Wade <jessica.wade@parameter1.com>
2022-11-08 08:19:44 -0600

    Create config folders and files

---
+++ sites/rentalproductnews.com/config/gam.js
@@ -0,0 +1,44 @@
+module.exports = {
+  basePath: 'forconstructionpros',
+  pathMaps: [
+    {
+      alias: 'default',
+      map: {
+        lb1: 'default/lb1',
+        'lb-sticky-bottom': 'default/lb1',
+        rail1: 'default/rail1',
+        rail2: 'default/rail1',
+        'infinite-rail': 'default/rail1',
+        'infinite-interstitial': 'default/rail1',
+        'in-content': 'default/rail1',
+        reskin: 'default/reskin',
+      },
+    },
+    {
+      alias: 'rental',
+      map: {
+        lb1: 'rental/lb1',
+        'lb-sticky-bottom': 'rental/lb1',
+        rail1: 'rental/rail1',
+        rail2: 'rental/rail1',
+        'infinite-rail': 'rental/rail1',
+        'infinite-interstitial': 'rental/rail1',
+        'in-content': 'rental/rail1',
+        reskin: 'rental/reskin',
+      },
+    },
+    {
+      alias: 'rental/lifting-equipment',
+      map: {
+        lb1: 'rental/lifting-equipment/lb1',
+        'lb-sticky-bottom': 'rental/lb1',
+        rail1: 'rental/lifting-equipment/rail1',
+        rail2: 'rental/lifting-equipment/rail1',
+        'infinite-rail': 'rental/lifting-equipment/rail1',
+        'infinite-interstitial': 'rental/lifting-equipment/rail1',
+        'in-content': 'rental/lifting-equipment/rail1',
+        reskin: 'rental/lifting-equipment/reskin',
+      },
+    },
+  ],
+};
