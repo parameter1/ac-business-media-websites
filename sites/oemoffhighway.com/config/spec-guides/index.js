3430ac52a080e39f5eeca648c9da4e51070f78ea
Shinsina <shinsina@att.net>
2024-02-14 14:26:02 -0600

    Remove extra spaces at end of description

--- sites/oemoffhighway.com/config/spec-guides/index.js
+++ sites/oemoffhighway.com/config/spec-guides/index.js
@@ -3,7 +3,7 @@ const engine = require('./engine');
 module.exports = {
   rootPath: '/specguide',
   title: 'OEM Off-Highway’s Engine Spec Guide',
-  description: '<em>OEM Off-Highway’s Engine Spec Guide</em> is an essential industry tool designed to help engineers specify the products they need for the next generation of heavy-duty on- and off-road mobile equipment. The Engine Spec Guide is the industry’s first and only completely interactive component specifier that was custom-built by engineers for engineers.  ',
+  description: '<em>OEM Off-Highway’s Engine Spec Guide</em> is an essential industry tool designed to help engineers specify the products they need for the next generation of heavy-duty on- and off-road mobile equipment. The Engine Spec Guide is the industry’s first and only completely interactive component specifier that was custom-built by engineers for engineers.',
   guides: {
     engine,
   },
