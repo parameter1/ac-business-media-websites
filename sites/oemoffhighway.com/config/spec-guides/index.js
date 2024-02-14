2b9f92ef7eae0229d4ebb8ac41da4dd7f0acb1f6
Shinsina <shinsina@att.net>
2024-02-14 14:24:32 -0600

    2024 OEM Off Highway Engine Spec Guide adjustments

--- sites/oemoffhighway.com/config/spec-guides/index.js
+++ sites/oemoffhighway.com/config/spec-guides/index.js
@@ -2,8 +2,8 @@ const engine = require('./engine');
 
 module.exports = {
   rootPath: '/specguide',
-  title: 'OEM Off-Highway Spec Guides',
-  description: '<em>OEM Off-Highway Spec Guides</em> are essential industry tools to help engineers specify the products they need for the next generation of heavy-duty on- and off-road mobile equipment. The Engine Spec Guide, Pump Spec Guide, and Motor Spec Guide are the industry’s first and only completely interactive component specifier that was custom built <strong>by engineers, for engineers.</strong>',
+  title: 'OEM Off-Highway’s Engine Spec Guide',
+  description: '<em>OEM Off-Highway’s Engine Spec Guide</em> is an essential industry tool designed to help engineers specify the products they need for the next generation of heavy-duty on- and off-road mobile equipment. The Engine Spec Guide is the industry’s first and only completely interactive component specifier that was custom-built by engineers for engineers.  ',
   guides: {
     engine,
   },
