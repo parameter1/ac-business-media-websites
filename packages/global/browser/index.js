7751e02e450c742b021884c98b2bff2d340f9c34
Brian Miller <brian@parameter1.com>
2024-02-26 10:15:54 -0600

    Fix & port remaining commone common comp to global
    Fix css import for spec guide and port company & section search compo css and support file to global theme. Try to remove dep on common package.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -1,16 +1,23 @@
 import GCSE from '@parameter1/base-cms-marko-web-gcse/browser';
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import Leaders from '@parameter1/base-cms-marko-web-leaders/browser';
-import Common from '@ac-business-media/package-common/browser';
 
+const GlobalCompanySearch = () => import(/* webpackChunkName: "global-company-search" */ './company-search.vue');
+const GlobalSectionSearch = () => import(/* webpackChunkName: "global-section-search" */ './section-search.vue');
 const GlobalSpecGuideTable = () => import(/* webpackChunkName: "global-spec-guide-table" */ './spec-guide/table.vue');
 const DynamicSiteMenuPositioner = () => import(/* webpackChunkName: "dynamic-site-menu-positioner" */ './dynamic-site-menu-positioner.vue');
 
 export default (Browser) => {
-  Common(Browser);
+  const { EventBus } = Browser;
   GCSE(Browser);
   MonoRail(Browser);
   Leaders(Browser);
   Browser.register('GlobalSpecGuideTable', GlobalSpecGuideTable);
   Browser.register('DynamicSiteMenuPositioner', DynamicSiteMenuPositioner);
+  Browser.register('GlobalCompanySearch', GlobalCompanySearch, {
+    provide: { EventBus },
+  });
+  Browser.register('GlobalSectionSearch', GlobalSectionSearch, {
+    provide: { EventBus },
+  });
 };
