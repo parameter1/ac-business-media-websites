d9a0627e1a96f933990cb2a3210820a2d42557ab
Brian Miller <brian@parameter1.com>
2024-02-29 12:12:56 -0600

    update section routes and add example profit matters config

--- sites/forconstructionpros-global.com/server/routes/website-section.js
+++ sites/forconstructionpros-global.com/server/routes/website-section.js
@@ -2,23 +2,17 @@ const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middlewar
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
 const blogs = require('@ac-business-media/package-global/templates/website-section/blogs');
 const webinars = require('@ac-business-media/package-global/templates/website-section/webinars');
-const podcasts = require('@ac-business-media/package-global/templates/website-section/podcasts');
 const upcomingEvents = require('@ac-business-media/package-global/templates/website-section/upcoming-events');
 const section = require('@ac-business-media/package-global/templates/website-section');
 const promoCards = require('@ac-business-media/package-global/templates/website-section/promo-cards');
 const sectionWithoutNativeX = require('@ac-business-media/package-global/templates/website-section/without-native-x');
 
 const withoutNativeXAlias = [
-  'events',
-  'expert-insights',
-  'expert-insights/white-papers',
+  'premium-content',
+  'white-papers',
 ];
 
 module.exports = (app) => {
-  app.get('/:alias(expert-insights/podcasts)', withWebsiteSection({
-    template: podcasts,
-    queryFragment,
-  }));
   app.get('/:alias(podcasts|videos)', withWebsiteSection({
     template: promoCards,
     queryFragment,
@@ -33,7 +27,7 @@ module.exports = (app) => {
       queryFragment,
     }));
   });
-  app.get('/:alias(events/event-calendar)', withWebsiteSection({
+  app.get('/:alias(events)', withWebsiteSection({
     template: upcomingEvents,
     queryFragment,
   }));
