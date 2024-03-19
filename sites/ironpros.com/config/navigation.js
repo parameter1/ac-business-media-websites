82b7e5b3921c323474972ca2a0b29d9b85742457
Brian Miller <brian@parameter1.com>
2024-03-19 13:35:38 -0500

    comment out contextual nav for now

--- sites/ironpros.com/config/navigation.js
+++ sites/ironpros.com/config/navigation.js
@@ -71,133 +71,133 @@ module.exports = {
   tertiary: {
     items: [],
   },
-  contexts: [
-    {
-      when: ['/equipment', '/trucks'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/trucks', label: 'Trucks' },
-          { href: '/equipment/earthmoving', label: 'Earthmoving' },
-          { href: '/equipment/fleet-maintenance', label: 'Fleet Maintenance' },
-          { href: '/equipment/attachments', label: 'Attachments' },
-          { href: '/equipment/compaction', label: 'Compaction' },
-          { href: '/equipment/crane', label: 'Crane' },
-        ],
-      },
-    },
-    {
-      when: ['/rental'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/rental/construction', label: 'Construction' },
-          { href: '/rental/general-tool', label: 'General Tool' },
-          { href: '/rental/lifting-equipment', label: 'Lifting Equipment' },
-          { href: '/rental/power', label: 'Power' },
-          { href: '/rental/rental-business', label: 'Rental Business' },
-          {
-            href: '/page/Subscribe-Links',
-            label: 'Rental Magazine ',
-            icon: 'book',
-            forceLabel: true,
-          },
-          {
-            href: 'https://acbusiness.dragonforms.com/loading.do?omedasite=RENTALprefs&pk=managepref',
-            label: 'Rental Newsletter',
-            icon: 'mail',
-            forceLabel: true,
-            target: '_blank',
-          },
-        ],
-      },
-    },
-    {
-      when: ['/concrete'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/concrete/decorative', label: 'Decorative' },
-          { href: '/concrete/equipment-products', label: 'Equipment & Products' },
-          { href: '/concrete/equipment-products/concrete-pumps', label: 'Pumps' },
-          { href: '/concrete/equipment-products/power-trowels', label: 'Trowels' },
-          { href: '/concrete/equipment-products/repair-rehabilitation-products', label: 'Repair' },
-          { href: '/concrete/world-of-concrete', label: 'World of Concrete' },
-        ],
-      },
-    },
-    {
-      when: ['/asphalt'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/asphalt/additives', label: 'Additives' },
-          { href: '/asphalt/plants', label: 'Plants' },
-          { href: '/asphalt/recycling', label: 'Recycling' },
-          { href: '/asphalt/pavers', label: 'Pavers' },
-          { href: '/asphalt/material-transfer-vehicles', label: 'MTV\'s' },
-        ],
-      },
-    },
-    {
-      when: ['/business'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/business/business-services', label: 'Services' },
-          { href: '/business/construction-safety', label: 'Safety' },
-          { href: '/business/business-services/training-education', label: 'Training & Education' },
-          { href: '/business/labor-workforce-development', label: 'Labor' },
-        ],
-      },
-    },
-    {
-      when: ['/construction-technology'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/construction-technology/apps', label: 'Apps' },
-          { href: '/construction-technology/theft-prevention', label: 'Theft Prevention' },
-          { href: '/construction-technology/personnel-management', label: 'Personnel Management' },
-          { href: '/construction-technology/equipment-monitoring-logistics', label: 'Equipment Monitoring' },
-          { href: '/construction-technology/machine-grade-control-gps-laser-other', label: 'Machine Grade Control, GPS, Laser & Other' },
-        ],
-      },
-    },
-    {
-      when: ['/pavement-maintenance'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/pavement-maintenance/sweepers', label: 'Sweepers' },
-          { href: '/pavement-maintenance/marking-striping', label: 'Marking & Striping' },
-          { href: '/pavement-maintenance/preservation-maintenance', label: 'Preservation' },
-        ],
-      },
-    },
-    {
-      when: ['/profit-matters'],
-      secondary: { items: topics },
-      tertiary: { items: [] },
-      primary: {
-        items: [
-          { href: '/profit-matters?contentTypes=Blog', label: 'Blogs' },
-          { href: '/profit-matters?contentTypes=Podcast', label: 'Podcasts' },
-          { href: '/profit-matters?contentTypes=Webinar', label: 'Webinars' },
-          { href: '/profit-matters?contentTypes=Video', label: 'Videos' },
-          { href: '/profit-matters?contentTypes=Article', label: 'Articles' },
-          { href: '/profit-matters/on-site-series', label: 'Contractor’s Site Report' },
-        ],
-      },
-    },
-  ],
+  // contexts: [
+  //   {
+  //     when: ['/equipment', '/trucks'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/trucks', label: 'Trucks' },
+  //         { href: '/equipment/earthmoving', label: 'Earthmoving' },
+  //         { href: '/equipment/fleet-maintenance', label: 'Fleet Maintenance' },
+  //         { href: '/equipment/attachments', label: 'Attachments' },
+  //         { href: '/equipment/compaction', label: 'Compaction' },
+  //         { href: '/equipment/crane', label: 'Crane' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/rental'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/rental/construction', label: 'Construction' },
+  //         { href: '/rental/general-tool', label: 'General Tool' },
+  //         { href: '/rental/lifting-equipment', label: 'Lifting Equipment' },
+  //         { href: '/rental/power', label: 'Power' },
+  //         { href: '/rental/rental-business', label: 'Rental Business' },
+  //         {
+  //           href: '/page/Subscribe-Links',
+  //           label: 'Rental Magazine ',
+  //           icon: 'book',
+  //           forceLabel: true,
+  //         },
+  //         {
+  //           href: 'https://acbusiness.dragonforms.com/loading.do?omedasite=RENTALprefs&pk=managepref',
+  //           label: 'Rental Newsletter',
+  //           icon: 'mail',
+  //           forceLabel: true,
+  //           target: '_blank',
+  //         },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/concrete'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/concrete/decorative', label: 'Decorative' },
+  //         { href: '/concrete/equipment-products', label: 'Equipment & Products' },
+  //         { href: '/concrete/equipment-products/concrete-pumps', label: 'Pumps' },
+  //         { href: '/concrete/equipment-products/power-trowels', label: 'Trowels' },
+  //         { href: '/concrete/equipment-products/repair-rehabilitation-products', label: 'Repair' },
+  //         { href: '/concrete/world-of-concrete', label: 'World of Concrete' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/asphalt'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/asphalt/additives', label: 'Additives' },
+  //         { href: '/asphalt/plants', label: 'Plants' },
+  //         { href: '/asphalt/recycling', label: 'Recycling' },
+  //         { href: '/asphalt/pavers', label: 'Pavers' },
+  //         { href: '/asphalt/material-transfer-vehicles', label: 'MTV\'s' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/business'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/business/business-services', label: 'Services' },
+  //         { href: '/business/construction-safety', label: 'Safety' },
+  //         { href: '/business/business-services/training-education', label: 'Training & Education' },
+  //         { href: '/business/labor-workforce-development', label: 'Labor' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/construction-technology'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/construction-technology/apps', label: 'Apps' },
+  //         { href: '/construction-technology/theft-prevention', label: 'Theft Prevention' },
+  //         { href: '/construction-technology/personnel-management', label: 'Personnel Management' },
+  //         { href: '/construction-technology/equipment-monitoring-logistics', label: 'Equipment Monitoring' },
+  //         { href: '/construction-technology/machine-grade-control-gps-laser-other', label: 'Machine Grade Control, GPS, Laser & Other' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/pavement-maintenance'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/pavement-maintenance/sweepers', label: 'Sweepers' },
+  //         { href: '/pavement-maintenance/marking-striping', label: 'Marking & Striping' },
+  //         { href: '/pavement-maintenance/preservation-maintenance', label: 'Preservation' },
+  //       ],
+  //     },
+  //   },
+  //   {
+  //     when: ['/profit-matters'],
+  //     secondary: { items: topics },
+  //     tertiary: { items: [] },
+  //     primary: {
+  //       items: [
+  //         { href: '/profit-matters?contentTypes=Blog', label: 'Blogs' },
+  //         { href: '/profit-matters?contentTypes=Podcast', label: 'Podcasts' },
+  //         { href: '/profit-matters?contentTypes=Webinar', label: 'Webinars' },
+  //         { href: '/profit-matters?contentTypes=Video', label: 'Videos' },
+  //         { href: '/profit-matters?contentTypes=Article', label: 'Articles' },
+  //         { href: '/profit-matters/on-site-series', label: 'Contractor’s Site Report' },
+  //       ],
+  //     },
+  //   },
+  // ],
   toggleMenu: {
     about: {
       // label: 'About',
