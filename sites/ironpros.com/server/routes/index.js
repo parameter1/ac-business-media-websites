92d38ca093aacd9cd69c4243f6a2ac3b6dccb4cb
Brian Miller <brian@parameter1.com>
2024-03-19 15:44:37 -0500

    Update Directory include

--- sites/ironpros.com/server/routes/index.js
+++ sites/ironpros.com/server/routes/index.js
@@ -20,13 +20,25 @@ module.exports = (app) => {
 
   // Directory Pages have to happen after content or they wont match
   directory(app, {
+    rootAlias: 'product-categories',
+    contentTypes: ['Company', 'Product'],
     assignedToWebsiteSectionIds: [
-      54289, // Asphalt
-      54300, // Business
-      54436, // Concrete
-      54319, // Construction Technology
-      54330, // Equipment
-      54468, // Pavement Maintenance
+      214237,
+      214238,
+      214239,
+      214240,
+      214241,
+      214242,
+      214243,
+      214244,
+      214245,
+      214246,
+      214247,
+      214248,
+      214249,
+      214250,
+      214251,
+      214252,
     ],
   });
 
