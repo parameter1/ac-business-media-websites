e139fc65bf85478e20767414facf9c831b43e265
Brian Miller <brian@parameter1.com>
2024-04-28 06:40:37 -0500

    register component

--- sites/ironpros.com/browser/index.js
+++ sites/ironpros.com/browser/index.js
@@ -1,10 +1,12 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 import Shared from '@ac-business-media/package-global/browser';
 
-const ImageSlider = () => import(/* webpackChunkName: "global-image-slider" */ './image-slider.vue');
+const ImageSlider = () => import(/* webpackChunkName: "site-image-slider" */ './image-slider.vue');
+const SpecComparisonTool = () => import(/* webpackChunkName: "site-spec-comparison-tool" */ './spec-comparison-tool.vue');
 
 Shared(Browser);
 
 Browser.register('SiteImageSlider', ImageSlider);
+Browser.register('SiteSpecComparison', SpecComparisonTool);
 
 export default Browser;
