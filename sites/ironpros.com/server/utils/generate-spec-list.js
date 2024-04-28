7b5e37ab39f314770844cfee8e4e737f705817f7
Brian Miller <brian@parameter1.com>
2024-04-28 06:52:22 -0500

    fix sytax issues

--- sites/ironpros.com/server/utils/generate-spec-list.js
+++ sites/ironpros.com/server/utils/generate-spec-list.js
@@ -10,13 +10,13 @@ const excludeKeys = [
 module.exports = ({ specsToAppend }) => {
   if (!specsToAppend) return [];
   const specs = [];
-  specsToAppend.forEach(({specs: s}) => {
-    s.forEach(({ key, label }) =>{
-      const existingSpec = specs.filter((s) => s.key === key).length > 0;
+  specsToAppend.forEach(({ specs: s }) => {
+    s.forEach(({ key, label }) => {
+      const existingSpec = specs.filter((t) => t.key === key).length > 0;
       if (!existingSpec && !excludeKeys.includes(key)) {
         specs.push({ key, label });
       }
     });
   });
-  return specs.sort((a, b) => a.label.localeCompare(b.label));;
+  return specs.sort((a, b) => a.label.localeCompare(b.label));
 };
