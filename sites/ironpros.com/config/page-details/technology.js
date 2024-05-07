d044a5586b905b947ad111588f7b676b231df684
Brian Miller <brian@parameter1.com>
2024-05-07 07:05:16 -0500

    UPdate img ref to be ironpros

--- sites/ironpros.com/config/page-details/technology.js
+++ sites/ironpros.com/config/page-details/technology.js
@@ -10,32 +10,32 @@ module.exports = {
         {
           name: 'Business Financial Tools',
           href: '/product-categories/technology-and-software/business-financial-tools',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/business_finance.6637bdb24afbf.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/business_finance.6637bdb24afbf.png',
         },
         {
           name: 'Communication Safety',
           href: '/product-categories/technology-and-software/communication-safety',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/comm_safety.6637bdd68b1fe.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/comm_safety.6637bdd68b1fe.png',
         },
         {
           name: 'Equipment Automation',
           href: 'product-categories/technology-and-software/equipment-automation',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/equip_automation.6637bd7115a29.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/equip_automation.6637bd7115a29.png',
         },
         {
           name: 'Marketplace',
           href: '/product-categories/technology-and-software/marketplace',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/marketplace.6637be06aa5d3.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/marketplace.6637be06aa5d3.png',
         },
         {
           name: 'Materials & Logistics Management',
           href: '/product-categories/technology-and-software/materials-logistics-management',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/materials_logistics.6637be7690c51.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/materials_logistics.6637be7690c51.png',
         },
         {
           name: 'Modeling and Design Tools',
           href: '/product-categories/technology-and-software/modeling-and-design-tools',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/modeling_design.6637bdac3efb8.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/modeling_design.6637bdac3efb8.png',
         },
         // {
         //   name: 'Other Technology Solutions',
@@ -45,12 +45,12 @@ module.exports = {
         {
           name: 'Site Data Capture',
           href: '/product-categories/technology-and-software/site-data-capture',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/site_capture_data.6637be737cef1.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/site_capture_data.6637be737cef1.png',
         },
         {
           name: 'Systems Integrators',
           href: '/product-categories/technology-and-software/systems-integrators',
-          src: 'https://img.forconstructionpros.com/files/base/acbm/fcp/image/2024/05/system_integrators.6637be95c5703.png',
+          src: 'https://img.ironpros.com/files/base/acbm/fcp/image/2024/05/system_integrators.6637be95c5703.png',
         },
       ],
     },
