feb7acc2923dab2b53d68feaa589c6edaab16335
Jessica Wade <jessica.wade@parameter1.com>
2024-05-07 00:27:29 -0500

    Create product TOC landing pages

---
+++ sites/ironpros.com/config/page-details/marketplace-rentals.js
@@ -0,0 +1,3 @@
+module.exports = {
+  introHTML: '<div><div class="node-list node-list--section-list">  <div class="node-list__header">    What is the IronPros Marketplace? </div>  <div class="node-list__body">    <p>Who has what construction equipment available to rent? These sites aggregate available equipment to help you find the heavy or light equipment required to be productive.</p></div></div></div>',
+};
---
+++ sites/ironpros.com/config/page-details/project-management-software.js
@@ -0,0 +1,3 @@
+module.exports = {
+  introHTML: '<div><div class="node-list node-list--section-list">  <div class="node-list__header">    What is Project Management Software?  </div>  <div class="node-list__body">    <p>The project-centric, anecdotal nature of construction is one reason it has been hard to automate. Construction project management software has evolved from static Gantt charts to dynamic resource management tools that rival ERP in their sophistication.</p><p>The most advanced project management software will encompass the entire quote-to-cash lifecycle. Others will be more purpose-built tools for a portion of the project, like planning or scheduling.</p></div></div></div>',
+};
