803c4254120bdb4d76b7ff6101e046070113351e
Shinsina <shinsina@att.net>
2024-09-27 09:58:59 -0500

    Utilize i18n configuration for adjusting Sponsored Content tagline

--- sites/forconstructionpros.com/index.js
+++ sites/forconstructionpros.com/index.js
@@ -3,6 +3,7 @@ const startServer = require('@ac-business-media/package-global/start-server');
 const routes = require('./server/routes');
 const siteConfig = require('./config/site');
 const coreConfig = require('./config/core');
+const i18n = require('./config/i18n');
 const { personasHandler } = require('./config/personas');
 
 const { log } = console;
@@ -13,4 +14,5 @@ module.exports = startServer({
   siteConfig,
   personasHandler,
   routes,
+  i18n: (v) => i18n[`${v}`.toLowerCase()] || v,
 }).then(() => log('Website started!')).catch((e) => setImmediate(() => { throw e; }));
