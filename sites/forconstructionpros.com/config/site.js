06a499e435f9ea877dd376a069e1b276f3c1cb7a
Brian Miller <brian@parameter1.com>
2024-12-10 13:31:47 -0600

    put him back

--- sites/forconstructionpros.com/config/site.js
+++ sites/forconstructionpros.com/config/site.js
@@ -64,6 +64,7 @@ module.exports = {
     },
     corporate: corporate.logo,
   },
+  useSectionLogos: true,
   tagline: ' ',
   socialMediaLinks: [
     { provider: 'facebook', href: 'https://www.facebook.com/ForConstructionPros', target: '_blank' },
